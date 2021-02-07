13e9afdf97c623a2bfdcff32d4805dcbbef03751
Sadeq Al-Dallal <66870615+sadeq93@users.noreply.github.com>
2021-02-07 11:01:19 +0100

    configed absolute path and added styled-components

---
+++ src/screens/BookmarksScreen.tsx
@@ -0,0 +1,11 @@
+import React, { FC ,ReactElement} from 'react';
+import  { Container,H1 } from 'src/library'
+export const BookmarksScreen:FC = () : ReactElement => {
+    return (
+        <Container>
+            <H1>Bookmarks Screen </H1>
+        </Container>
+    )
+}
+
+
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import React, { FC ,ReactElement} from 'react';
+import  { Container,H1 } from 'src/library'
+export const HomeScreen:FC = () : ReactElement => {
+    return (
+        <Container>
+            <H1>Home Screen </H1>
+        </Container>
+    )
+}
+
+
