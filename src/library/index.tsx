153c827350a4d329c0b07da5d15fba79d2a98a3a
Sadeq Al-Dallal <66870615+sadeq93@users.noreply.github.com>
2021-02-07 12:33:31 +0100

    added react-native-victors-icons

--- src/library/index.tsx
+++ src/library/index.tsx
@@ -1,9 +1,10 @@
+// This is my Custom Library 
 import React, { FC ,ReactElement} from 'react';
 import styled from 'styled-components/native';
 
 // ---------------------------------------------------------- Container 
 interface Props{
-    children:ReactElement
+    children:ReactElement[]
 }
  const Container :FC <Props> = ({children}:Props):ReactElement  => {
   return <StyledContainer>{children}</StyledContainer>;
@@ -32,5 +33,26 @@ const H4 = styled.Text`
 const P = styled.Text`
   font-size: 14px;
 `;
+// ---------------------------------------------------------- Icons
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+interface IconsInterface {
+  home : ReactElement
+  home_o : ReactElement
+  bookmark : ReactElement
+  bookmark_o : ReactElement
+  edit : ReactElement
+  deleteIcon : ReactElement
+}
+const Icons :IconsInterface = {
+  home : <Ionicons name="home" size={30} color="#000" />,
+  home_o : <Ionicons name="home-outline" size={30} color="#000" />,
+  bookmark : <FontAwesome name="bookmark" size={30} color="#000" />,
+  bookmark_o : <FontAwesome name="bookmark-o" size={30} color="#000" />,
+  edit : <FontAwesome name="edit" size={30} color="#000" />,
+  deleteIcon : <MaterialCommunityIcons name="delete" size={30} color="#000" />
+}
 // ----------------------------------------------------------
-export {Container, H1, H2, H3, H4, P}
+
+export {Container, H1, H2, H3, H4, P,Icons}
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,10 @@
 import React, { FC ,ReactElement} from 'react';
-import  { Container,H1 } from 'src/library'
+import  { Container,H1,Icons } from 'src/library'
 export const HomeScreen:FC = () : ReactElement => {
     return (
         <Container>
             <H1>Home Screen </H1>
+            {Icons.home}
         </Container>
     )
 }
